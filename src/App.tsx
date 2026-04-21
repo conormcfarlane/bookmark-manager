86510a24ca7c7c82bb74217f372c516d19a09043
conormcfarlane <conormcf98@gmail.com>
2026-04-21 19:56:52 +1000

    added env locl file , all json now being read and fetched from supabase insetad of locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bookmarks from "./assets/components/Bookmarks";
 import Header from "./assets/components/Header";
 import SideBar from "./assets/components/SideBar";
-import bookmarkDataJson from "./assets/data/data.json";
-import type { BookmarkData } from "./assets/types/bookmarkTypes";
+import type { Bookmark } from "./assets/types/bookmarkTypes";
+import { supabase } from "./lib/supabaseClient";
 
 function App() {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>("");
-  const bookmarkData: BookmarkData = bookmarkDataJson;
-  const filteredBookmarks = bookmarkData.bookmarks.filter((bookmark) => {
+
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    const fetchBookmarks = async () => {
+      const { data, error } = await supabase.from('bookmarks').select('*');
+      console.log(data)
+
+      if (error) {
+        console.log(error);
+        return;
+      }
+      setBookmarks(data as Bookmark[]);
+    };
+    fetchBookmarks();
+
+  }, [])
+
+  const filteredBookmarks: Bookmark[] = bookmarks.filter((bookmark) => {
     const matchesTags =
       selectedTags.length === 0 ||
       selectedTags.every((tag) => bookmark.tags.includes(tag));
